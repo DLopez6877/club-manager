604e4eac2f8c75de3138449729e82a54a1ae1cf5
Daniel <dlopez6877@gmail.com>
2017-07-14 11:39:34 -0700

    setup member structure and about to setup service

--- src/app/club/club.component.ts
+++ src/app/club/club.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Members } from '../members.model';
+import { Router } from '@angular/router';
+import { MemberService } from './../member.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
-  selector: 'app-club',
-  templateUrl: './club.component.html',
-  styleUrls: ['./club.component.scss']
+  selector: 'app-marketplace',
+  templateUrl: './marketplace.component.html',
+  styleUrls: ['./marketplace.component.css'],
+  providers: [MemberService]
 })
-export class ClubComponent implements OnInit {
 
-  constructor() { }
+
+export class MarketplaceComponent implements OnInit {
+  members: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
+
+  constructor(private router: Router, private memberService: MemberService){}
 
   ngOnInit() {
+    this.members = this.memberService.getMembers();
   }
 
+  goToDetailPage(clickedMember) {
+      this.router.navigate(['members', clickedMember.$key]);
+  };
 }
